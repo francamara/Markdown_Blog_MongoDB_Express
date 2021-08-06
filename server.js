81b78a5b1b02b513c74ab7f62976fb219f2fc064
francamaradev <francamaradev@gmail.com>
2021-08-06 18:02:29 -0300

    added more stuff (27:18)

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const exprees = require('express')
+const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 
 require('dotenv').config()
 
+DB_NAME = process.env.DB_NAME
+
+// console.log(DB_NAME)
+
+mongoose.connect(`mongodb://localhost:${DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
 const port = process.env.PORT || 3000
 
-const app = exprees()
+const app = express()
 
 app.set('view engine', 'ejs')
 
@@ -15,10 +25,12 @@ app.get('/', (req, res) => {
   const articles = [
     {
       title: 'Test Article',
-      description: 'Test Article',
+      date: new Date(),
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, facere aut! Recusandae impedit, dolore facere culpa praesentium eum vitae hic deleniti officiis, perferendis repellat quae autem amet aliquam incidunt ipsum.',
     },
   ]
-  res.render('index', { articles: articles })
+  res.render('articles/index', { articles: articles })
 })
 
 app.listen(port)
